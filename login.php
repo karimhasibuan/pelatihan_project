3fb41d819ec0250f14538961a8f312b0697e76a1
Karim Hasibuan <amfibicoding19@gmail.com>
2023-08-14 07:57:54 +0700

    feat: create login page.

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="ceklogin.php" method="post">
+        <input type="text" name="username" id="username">
+        <input type="password" name="password" id="password">
+        <button type="submit">MASUK</button>
+    </form>
+</body>
+
+</html>
